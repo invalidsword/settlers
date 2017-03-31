1c513233bc528a5817c605422436730d5d591b65
GECote <georges-emile.cote@mail.mcgill.ca>
2017-03-31 16:45:08 -0400

    Revert "Backend v2.0"

--- src/main/java/com/example/models/gameModels/MaritimeTrade.java
+++
@@ -1,9 +0,0 @@
-package com.example.models.gameModels;
-
-import java.util.HashMap;
-
-public class MaritimeTrade implements Trade {
-    public MaritimeTrade(Player pTrader1, StealableCard.Resource pResource, int pAmount){
-
-    }
-}
--- src/main/java/com/example/models/gameModels/Trade.java
+++
@@ -1,7 +0,0 @@
-package com.example.models.gameModels;
-
-/**
- * Created by Ming-PC on 3/26/2017.
- */
-public interface Trade {
-}
